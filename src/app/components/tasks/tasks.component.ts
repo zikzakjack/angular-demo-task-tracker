de0128580a4aee88b3f4b0a1d733c862efed0025
zikzakjack <kalyansmiles@gmail.com>
2021-08-24 00:10:34 +0530

    wired TaskService in Tasks Component

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/Task';
-import { TASKS } from 'src/app/mock-tasks';
+import { TaskService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.tasks = this.taskService.getTasks()
   }
 
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TASKS } from 'src/app/mock-tasks';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,8 @@ import { Injectable } from '@angular/core';
 export class TaskService {
 
   constructor() { }
+
+  getTasks(): Task[] {
+    return TASKS;
+  }
 }
