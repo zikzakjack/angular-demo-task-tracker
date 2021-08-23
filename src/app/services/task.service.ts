de8e97123a228badcdd572fe3386e35dbd00ba4a
zikzakjack <kalyansmiles@gmail.com>
2021-08-24 01:31:47 +0530

    fetching tasks from backend service

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs';
 import { Task } from 'src/app/Task';
-import { TASKS } from 'src/app/mock-tasks';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = "http://localhost:5000/tasks";
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
